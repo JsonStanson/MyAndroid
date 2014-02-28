95d1d3b15227c63a3e8d77dceee211869d6649a1
WizardAdrian <195686001@qq.com>
2014-02-28 11:08:06 +0800

    Create EncryptToolsMD5.java
    MD5 tool
    
    Need some customization when you use it.

---
+++ EncryptToolsMD5.java
@@ -0,0 +1,79 @@
+public class EncryptTools {
+	public static String MD5(String str) {
+		MessageDigest md5 = null;
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+		} catch (Exception e) {
+			e.printStackTrace();
+			return "";
+		}
+		char[] charArray = str.toCharArray();
+		byte[] byteArray = new byte[charArray.length];
+		for (int i = 0; i < charArray.length; i++) {
+			byteArray[i] = (byte) charArray[i];
+		}
+		byte[] md5Bytes = md5.digest(byteArray);
+		StringBuffer hexValue = new StringBuffer();
+		for (int i = 0; i < md5Bytes.length; i++) {
+			int val = ((int) md5Bytes[i]) & 0xff;
+			if (val < 16) {
+				hexValue.append("0");
+			}
+			hexValue.append(Integer.toHexString(val));
+		}
+		return hexValue.toString();
+	}
+
+	public static String Encrypt(String strSrc, String encName) {
+		MessageDigest md = null;
+		String strDes = null;
+		byte[] bt = strSrc.getBytes();
+		try {
+			if (TextUtils.isEmpty(encName)) {
+				encName = "SHA-256";
+			}
+			md = MessageDigest.getInstance(encName);
+			md.update(bt);
+			// strDes = bin2hex(md.digest());
+			// strDes = bin2hex(strDes.getBytes());
+			strDes = bytes2Hex(md.digest());
+			strDes = bytes2Hex(strDes.getBytes());
+		} catch (NoSuchAlgorithmException e) {
+			return null;
+		}
+		return strDes;
+	}
+
+	public static String bytes2Hex(byte[] bts) {
+		String des = "";
+		String tmp = null;
+		for (int i = 0; i < bts.length; i++) {
+			tmp = (Integer.toHexString(bts[i] & 0xFF));
+			if (tmp.length() == 1) {
+				des += "0";
+			}
+			des += tmp;
+		}
+		return des;
+	}
+
+	public static String bin2hex(byte[] data) {
+		return String.format("%0" + (data.length * 2) + "X", new BigInteger(1,
+				data));
+	}
+
+	/**
+	 * not used
+	 * */
+	public static byte[] getHash(String password) {
+		MessageDigest digest = null;
+		try {
+			digest = MessageDigest.getInstance("SHA-256");
+		} catch (NoSuchAlgorithmException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		digest.reset();
+		return digest.digest(password.getBytes());
+	}
+}
